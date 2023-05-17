44fffabf09f21c781051b1e96d35fa64cf5c5596
clinton <clinton.wambua@student.moringaschool.com>
2023-05-17 08:46:08 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+#Test 1
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#Test 2
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+#Test 3
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+#Test 4
+def add(num1, num2)
+    return num1 + num2
+end
+
+#Test 5
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2
+end
